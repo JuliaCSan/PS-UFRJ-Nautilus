e9e14b9b13afd4cec2a5752505fec96087661ed1
JuliaCSan <75736421+JuliaCSan@users.noreply.github.com>
2023-03-14 23:22:49 -0300

    Criado Exe03.py

---
+++ PYTHON: Basico/Exe03.py
@@ -0,0 +1,15 @@
+for c in range(1, 1001):
+    print('{}'.format(c))
+    
+    
+print('=='*40)
+for p in range(1, 1001):
+    if p%2==0:
+        print(p, ['PAR'] )
+        
+print('=='*40)
+   
+
+for i in range(1, 1001):
+    if i%2==1:
+         print(i, ['ÍMPAR'])
