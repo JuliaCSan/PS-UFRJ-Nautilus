ca7b59ca43eda0e0685ed82613cfad8fcb348fda
JuliaCSan <75736421+JuliaCSan@users.noreply.github.com>
2023-03-14 23:20:58 -0300

    Criado Exe01.py

---
+++ PYTHON: Basico/Exe01.py
@@ -0,0 +1,11 @@
+exe01:
+
+nota1 = float(input('Escreva a nota 1: '))
+nota2 = float(input('Escreva a nota 2: '))
+nota3 = float(input('Escreva a nota 3: '))
+media = (nota1 + nota2 + nota3)/3
+print('Media:', media)
+
+if media >=7.0:
+    print('Situação do aluno: Aprovado')
+else: print('Situação do aluno: Reprovado') 
